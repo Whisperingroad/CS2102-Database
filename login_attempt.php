c0beaa481ab94a38cbdf1606a9b3a71ec8aba634
Sebastian Wong <sebastian.wong@hotmail.com>
2015-03-20 16:31:44 +0800

    separated connection scripts with login scripts

--- login_attempt.php
+++ login_attempt.php
@@ -1,23 +1,8 @@
 <?php
-/* Set oracle user login and password info */
 
-$host="sid3.comp.nus.edu.sg"; // Host name 
-$username="a0101856"; // Mysql username 
-$password="crse1420"; // Mysql password 
-$db_name="sid3.comp.nus.edu.sg"; // Database name 
-$tbl_name="REGISTERED_USER"; // Table name 
 
-// Connect to server and select databse.
-//$connect = @ocilogon($username, $password, $db_name);
-putenv('ORACLE_HOME=/oraclient');
-$dbh = ocilogon('a0101856', 'crse1420', ' (DESCRIPTION =
-(ADDRESS_LIST =
-(ADDRESS = (PROTOCOL = TCP)(HOST = sid3.comp.nus.edu.sg)(PORT = 1521))
-)
-(CONNECT_DATA =
-(SERVICE_NAME = sid3.comp.nus.edu.sg)
-)
-)');
+
+include 'connection.php';
 if (!$dbh)  {
     echo "An error occurred connecting to the database"; 
     exit; 
@@ -25,6 +10,7 @@ if (!$dbh)  {
 
 $myusername=$_POST['myusername']; 
 $mypassword=$_POST['mypassword'];  
+
 
 $stid = oci_parse($dbh, "SELECT * FROM REGISTERED_USER where  username='$myusername' and userpassword='$mypassword' ");
 oci_execute($stid);
