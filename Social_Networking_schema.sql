4db7ef9220033588c4ea29fd22c65bbc664ff624
Sebastian Wong <sebastian.wong@hotmail.com>
2015-03-30 15:06:37 +0800

    Updates

--- Social_Networking_schema.sql
+++ Social_Networking_schema.sql
@@ -1,27 +1,34 @@
+
 CREATE TABLE REGISTERED_USER(
-reputation INT,
-email VARCHAR(64),
-userPassword VARCHAR(64),
-username VARCHAR(32) PRIMARY KEY
+email VARCHAR(64) NOT NULL UNIQUE,
+userPassword VARCHAR(64) NOT NULL,
+reputation INT DEFAULT '0',
+username VARCHAR(32) PRIMARY KEY,
+isAdmin CHAR(1) DEFAULT 'N' Check(isAdmin = 'Y' or isAdmin = 'N')
 );
 
+
 CREATE TABLE post_writepost (
-post_time TIMESTAMP,
-post_title VARCHAR(32),
+post_content VARCHAR(4000) NOT NULL,
+post_time TIMESTAMP with local time zone DEFAULT CURRENT_TIMESTAMP,
+post_title VARCHAR(32) NOT NULL,
 post_username VARCHAR(32) REFERENCES REGISTERED_USER(USERNAME),
 PRIMARY KEY (post_time, post_title, post_username)
 );
+
+
 CREATE TABLE contains_comments(
-count_votes INT,
-comment_username VARCHAR(32),
-comment_content VARCHAR(128), 
-comment_time TIMESTAMP,
+count_votes INT default '0',
+comment_username VARCHAR(32) NOT NULL,
+comment_content VARCHAR(128) NOT NULL, 
+comment_time TIMESTAMP with local time zone DEFAULT CURRENT_TIMESTAMP,
 post_username VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP,
+post_time TIMESTAMP with local time zone,
 FOREIGN KEY (post_username, post_title, post_time) REFERENCES post_writepost( post_username, post_title, post_time),
 PRIMARY KEY (post_username, post_title, post_time, comment_username, comment_time)
 ); 
+
 
 CREATE TABLE write_comments(
 comment_username VARCHAR(32), 
@@ -68,3 +75,5 @@ FOREIGN KEY (vote_username) REFERENCES Registered_User(username),
 FOREIGN KEY (comment_time, comment_username, post_username, post_title, post_time) references write_comments(comment_time, comment_username, post_username, post_title, post_time),
 PRIMARY KEY (comment_username, comment_time, post_username, post_title, post_time, vote_username, hasVoted)
 );
+
+
