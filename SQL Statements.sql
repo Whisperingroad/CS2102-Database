9cef692ad4195feef5816cfe8eafa621f1fc3628
@r-o-n-n <tanronney@users.noreply.github.com>
2015-03-30 15:32:07 +0800

    Update SQL Statements.sql

--- SQL Statements.sql
+++ SQL Statements.sql
@@ -1,3 +1,11 @@
+--login
+SELECT * FROM REGISTERED_USER where  username='$myusername' and userpassword='$mypassword'
+
+--homepage (display all the posts)
+  --what's new (sort by date)
+  SELECT * FROM 
+  --what's hot (sort by votes)
+
 --test statements
 insert into Registered_User (email,userPassword,username) values('sebastian.wong@hotmail.com', 'password123', 'sebbysebseb');
 insert into Registered_User (email,userPassword,username) values('chewyxiu@hotmail.com', 'chewyxiu', 'chewyxiu');
@@ -16,5 +24,3 @@ insert into post_writepost(post_title, post_content, post_username) values ('Int
 insert into post_writepost(post_title, post_content, post_username) values ('Introduction post','Hello, this is my first post in social networking site, thank you', 'sebbysebseb');
 select * from post_writepost;
 DROP table post_writepost;
-
---test
