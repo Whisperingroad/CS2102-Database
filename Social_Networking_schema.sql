e684f6781a8eea6c10f30a504d37558f003a9a28
Whisperingroad <sebastian.wong@hotmail.com>
2015-04-07 03:50:25 +0800

    Reduced timestamp precision. Cascaded delete statements

--- Social_Networking_schema.sql
+++ Social_Networking_schema.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE REGISTERED_USER(
 email VARCHAR(64) NOT NULL UNIQUE,
 userPassword VARCHAR(64) NOT NULL,
@@ -10,8 +9,8 @@ isAdmin CHAR(1) DEFAULT 'N' Check(isAdmin = 'Y' or isAdmin = 'N')
 
 CREATE TABLE post_writepost (
 post_content VARCHAR(4000) NOT NULL,
-post_time TIMESTAMP [(0)] with local time zone DEFAULT CURRENT_TIMESTAMP,
-post_topic VARCHAR(32)	NOT NULL,
+post_time TIMESTAMP(0) with local time zone DEFAULT CURRENT_TIMESTAMP,
+--post_topic VARCHAR(32)	NOT NULL,
 post_title VARCHAR(32) NOT NULL,
 post_username VARCHAR(32) REFERENCES REGISTERED_USER(USERNAME),
 pvotes INT default '0',
@@ -23,32 +22,32 @@ CREATE TABLE contains_comments(
 count_votes INT default '0',
 comment_username VARCHAR(32) NOT NULL,
 comment_content VARCHAR(128) NOT NULL, 
-comment_time TIMESTAMP [(0)],
+comment_time TIMESTAMP(0) with local time zone DEFAULT CURRENT_TIMESTAMP,
 post_username VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP [(0)],
-FOREIGN KEY (post_username, post_title, post_time) REFERENCES post_writepost( post_username, post_title, post_time),
+post_time TIMESTAMP(0) with local time zone,
+FOREIGN KEY (post_username, post_title, post_time) REFERENCES post_writepost( post_username, post_title, post_time) ON DELETE CASCADE,
 PRIMARY KEY (post_username, post_title, post_time, comment_username, comment_time)
 ); 
 
 
 CREATE TABLE write_comments(
 comment_username VARCHAR(32), 
-comment_time TIMESTAMP [(0)] with local time zone DEFAULT CURRENT_TIMESTAMP,
+comment_time TIMESTAMP(0) with local time zone,
 post_username VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP [(0)],
-FOREIGN KEY (comment_username, comment_time,post_username, post_title, post_time) REFERENCES contains_comments (comment_username, comment_time, post_username, post_title, post_time),
+post_time TIMESTAMP(0) with local time zone,
+FOREIGN KEY (comment_username, comment_time,post_username, post_title, post_time) REFERENCES contains_comments (comment_username, comment_time, post_username, post_title, post_time) ON DELETE CASCADE,
 PRIMARY KEY (comment_username, comment_time, post_username, post_title, post_time)
 ); 
 
 CREATE TABLE vote_post(
 hasVoted CHAR(1) CHECK (hasVoted = 'Y' or hasVoted = 'N'),
 voter_username VARCHAR(32), 
-post_time  TIMESTAMP [(0)],
+post_time  TIMESTAMP(0) with local time zone,
 post_title VARCHAR(32),
 post_username VARCHAR(32),
-FOREIGN KEY(post_time,post_title, post_username) REFERENCES post_writepost(  post_time,post_title, post_username),
+FOREIGN KEY(post_time,post_title, post_username) REFERENCES post_writepost(  post_time,post_title, post_username) ON DELETE CASCADE,
 FOREIGN KEY(voter_username) REFERENCES REGISTERED_USER(username),
 PRIMARY KEY(post_time, post_title, post_username, voter_username)
 );
@@ -59,23 +58,21 @@ topic_title VARCHAR(32) PRIMARY KEY);
 CREATE TABLE classifiedBy(
 topic_title VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP [(0)],
+post_time TIMESTAMP(0) with local time zone,
 post_username VARCHAR(32),
 FOREIGN KEY (topic_title) REFERENCES  Topic(topic_title),
-FOREIGN KEY (post_title, post_time,post_username) REFERENCES post_writepost(post_title, post_time,post_username), 
+FOREIGN KEY (post_title, post_time,post_username) REFERENCES post_writepost(post_title, post_time,post_username) ON DELETE CASCADE, 
 PRIMARY KEY (topic_title,post_title, post_time,post_username));
 
 CREATE TABLE vote_comments(
 hasVoted CHAR(1) CHECK(hasVoted = 'Y' or hasVoted = 'N'),
 post_username VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP [(0)],
-comment_time TIMESTAMP [(0)],
+post_time TIMESTAMP(0) with local time zone,
+comment_time TIMESTAMP(0) with local time zone,
 comment_username VARCHAR(32),
 voter_username VARCHAR(32),
 FOREIGN KEY (voter_username) REFERENCES Registered_User(username),
-FOREIGN KEY (comment_time, comment_username, post_username, post_title, post_time) references write_comments(comment_time, comment_username, post_username, post_title, post_time),
+FOREIGN KEY (comment_time, comment_username, post_username, post_title, post_time) references write_comments(comment_time, comment_username, post_username, post_title, post_time) ON DELETE CASCADE,
 PRIMARY KEY (comment_username, comment_time, post_username, post_title, post_time, voter_username, hasVoted)
 );
-
-
