9ebec3c326a1d747c6b5eac7c9f7beec09043d3b
Sebastian Wong <sebastian.wong@hotmail.com>
2015-04-06 15:55:48 +0800

    Corrected timestamp precision and inserted missing attributes.
    Please verify

--- Social_Networking_schema.sql
+++ Social_Networking_schema.sql
@@ -10,10 +10,11 @@ isAdmin CHAR(1) DEFAULT 'N' Check(isAdmin = 'Y' or isAdmin = 'N')
 
 CREATE TABLE post_writepost (
 post_content VARCHAR(4000) NOT NULL,
-post_time TIMESTAMP with local time zone DEFAULT CURRENT_TIMESTAMP,
+post_time TIMESTAMP [(0)] with local time zone DEFAULT CURRENT_TIMESTAMP,
+post_topic VARCHAR(32)	NOT NULL,
 post_title VARCHAR(32) NOT NULL,
 post_username VARCHAR(32) REFERENCES REGISTERED_USER(USERNAME),
-pvotes INT,
+pvotes INT default '0',
 PRIMARY KEY (post_time, post_title, post_username)
 );
 
@@ -22,10 +23,10 @@ CREATE TABLE contains_comments(
 count_votes INT default '0',
 comment_username VARCHAR(32) NOT NULL,
 comment_content VARCHAR(128) NOT NULL, 
-comment_time TIMESTAMP with local time zone DEFAULT CURRENT_TIMESTAMP,
+comment_time TIMESTAMP [(0)],
 post_username VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP with local time zone,
+post_time TIMESTAMP [(0)],
 FOREIGN KEY (post_username, post_title, post_time) REFERENCES post_writepost( post_username, post_title, post_time),
 PRIMARY KEY (post_username, post_title, post_time, comment_username, comment_time)
 ); 
@@ -33,10 +34,10 @@ PRIMARY KEY (post_username, post_title, post_time, comment_username, comment_tim
 
 CREATE TABLE write_comments(
 comment_username VARCHAR(32), 
-comment_time TIMESTAMP,
+comment_time TIMESTAMP [(0)] with local time zone DEFAULT CURRENT_TIMESTAMP,
 post_username VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP,
+post_time TIMESTAMP [(0)],
 FOREIGN KEY (comment_username, comment_time,post_username, post_title, post_time) REFERENCES contains_comments (comment_username, comment_time, post_username, post_title, post_time),
 PRIMARY KEY (comment_username, comment_time, post_username, post_title, post_time)
 ); 
@@ -44,12 +45,12 @@ PRIMARY KEY (comment_username, comment_time, post_username, post_title, post_tim
 CREATE TABLE vote_post(
 hasVoted CHAR(1) CHECK (hasVoted = 'Y' or hasVoted = 'N'),
 voter_username VARCHAR(32), 
-post_time  TIMESTAMP,
+post_time  TIMESTAMP [(0)],
 post_title VARCHAR(32),
 post_username VARCHAR(32),
 FOREIGN KEY(post_time,post_title, post_username) REFERENCES post_writepost(  post_time,post_title, post_username),
 FOREIGN KEY(voter_username) REFERENCES REGISTERED_USER(username),
-PRIMARY KEY(post_time, post_title, post_username)
+PRIMARY KEY(post_time, post_title, post_username, voter_username)
 );
 
 CREATE TABLE Topic(
@@ -58,7 +59,7 @@ topic_title VARCHAR(32) PRIMARY KEY);
 CREATE TABLE classifiedBy(
 topic_title VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP,
+post_time TIMESTAMP [(0)],
 post_username VARCHAR(32),
 FOREIGN KEY (topic_title) REFERENCES  Topic(topic_title),
 FOREIGN KEY (post_title, post_time,post_username) REFERENCES post_writepost(post_title, post_time,post_username), 
@@ -68,13 +69,13 @@ CREATE TABLE vote_comments(
 hasVoted CHAR(1) CHECK(hasVoted = 'Y' or hasVoted = 'N'),
 post_username VARCHAR(32),
 post_title VARCHAR(32),
-post_time TIMESTAMP,
-comment_time TIMESTAMP,
+post_time TIMESTAMP [(0)],
+comment_time TIMESTAMP [(0)],
 comment_username VARCHAR(32),
-vote_username VARCHAR(32),
-FOREIGN KEY (vote_username) REFERENCES Registered_User(username),
+voter_username VARCHAR(32),
+FOREIGN KEY (voter_username) REFERENCES Registered_User(username),
 FOREIGN KEY (comment_time, comment_username, post_username, post_title, post_time) references write_comments(comment_time, comment_username, post_username, post_title, post_time),
-PRIMARY KEY (comment_username, comment_time, post_username, post_title, post_time, vote_username, hasVoted)
+PRIMARY KEY (comment_username, comment_time, post_username, post_title, post_time, voter_username, hasVoted)
 );
 
 
