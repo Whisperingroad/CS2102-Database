37ccbc8898f53c9328877f1bc0ac1cc77a4bc9a1
HipHopAllen <liuy0095@e.ntu.edu.sg>
2015-04-09 05:30:55 +0800

    Create SQL Statements 2.sql
    Updating a particular post
    Voting a post
    Voting a comment
    Decreasing the vote of a post
    Decreasing the vote of a comment

---
+++ SQL Statements 2.sql
@@ -0,0 +1,40 @@
+--Needs sub queries for each
+
+------------------------
+
+--test statements
+--Updating a particular post
+  UPDATE post_writepost SET post_content = '' and post_title = '' WHERE post_username LIKE '' and post_time LIKE '';
+
+-------------------------------------------------
+
+--test statements
+--voting post
+  insert into vote_post(hasVoted,voter_username,post_username,post_title, post_time) values ('Y','sebbysebseb','sebbysebseb','Introduction post','15/01/01 01:25:25')
+  
+  UPDATE post_writepost SET pvotes = pvotes+1 WHERE post_username LIKE '' and post_time LIKE '';
+
+--test statements
+--voting comment
+  insert into vote_comment(hasVoted,voter_username,comment_time,comment_username,post_title,post_username,post_time) 
+  values ('Y','sebbysebseb','15/01/01 01:25:25','sebbysebseb','Introduction post','sebbysebseb','15/01/01 01:25:25');
+  
+  UPDATE contains_comments SET count_votes = count_votes+1 WHERE comment_username LIKE '' and comment_time LIKE '';
+
+------------------------------------------------
+  
+--test statements
+--Decreasing the vote of a post
+--insert into vote_post(hasVoted,voter_username,post_username,post_title,post_time) values ('N','sebbysebseb','sebbysebseb','Introduction post','15/01/01 01:25:25')
+  UPDATE vote_post SET hasVoted = 'N' WHERE post_time = '' and post_username = '' and voter_username = '';
+  
+  UPDATE post_writepost SET pvotes = pvotes-1 WHERE post_username LIKE '' and post_time LIKE '';
+
+--test statements
+--Decreasing the vote of a comment
+--insert into vote_comment(hasVoted,voter_username,comment_time,comment_username,post_title,post_username,post_time) 
+--values ('N','sebbysebseb','15/01/01 01:25:25','sebbysebseb','Introduction post','sebbysebseb','15/01/01 01:25:25');
+  UPDATE vote_comment SET hasVoted = 'N' WHERE comment_username LIKE '' and comment_time LIKE '' and voter_username = '';
+  
+  UPDATE contains_comments SET count_votes = count_votes-1 WHERE comment_username LIKE '' and comment_time LIKE '';
+  
