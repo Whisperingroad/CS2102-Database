6e7099ee852a8df710222c107c7674836119d968
Whisperingroad <sebastian.wong@hotmail.com>
2015-04-14 03:36:57 +0800

    trial

--- scripts/Profile.php
+++ scripts/Profile.php
@@ -26,12 +26,52 @@ if (!$dbh)  {
     while ($row = oci_fetch_array($stid1,OCI_DEFAULT)) {
       echo "<ul class =". "HotPostsList".">";  
       echo "<li id ="."post_title". ">". $row[POST_TITLE]. "</li>";
+
+      //get user's reputation
+      $sql = "SELECT reputation FROM REGISTERED_USER WHERE username = '". $row[POST_USERNAME]."'";
+      $stid = oci_parse($dbh, $sql);
+      oci_execute($stid);
+      $row1 = oci_fetch_array($stid,OCI_DEFAULT);
+      oci_free_statement($stid);
+
       echo "<li id =". "post_content".">".$row[POST_CONTENT]. "</li>";
-      echo "<li id =". "post_username".">Authored by " .$row[POST_USERNAME]. "</li>";  
+      echo "<li id =". "post_username".">Authored by " .$row[POST_USERNAME].' '.$row1[0].' Points</li>';
       echo "<li id =". "post_time".">".  $row[POST_TIME] . "</li>";
       echo "<li id =". "postvotes". ">". $row[PVOTES]."</li>";      
-      echo "</ul>";  
-      
+      echo "</ul>";
+
+      //comment button
+      echo '<table>';
+      echo '<form method="post"action="http://cs2102-i.comp.nus.edu.sg/~a0101856/cs2102/scripts/Comment.php">
+     <input type="hidden" name="post_title" value= "'.$row[POST_TITLE].'">
+     <input type="hidden" name="post_time" value="'.$row[POST_TIME].'">
+     <input type="hidden" name="post_username" value="'.$row[POST_USERNAME].'">
+         <td><input type="submit" value = "Comment"></form></td>';
+
+       $sql = "SELECT isAdmin FROM REGISTERED_USER WHERE username = '".$_SESSION['myusername']."'";
+    $stid = oci_parse($dbh, $sql);
+        oci_execute($stid);
+        $row1 = oci_fetch_array($stid,OCI_DEFAULT);
+        oci_free_statement($stid); 
+
+         //edit or delete if you are post author or admin
+         if ( $_SESSION['myusername'] == $row[POST_USERNAME] || $row1[0] == 'Y'){ 
+         //Edit button
+         echo '<form method="post"action="http://cs2102-i.comp.nus.edu.sg/~a0101856/cs2102/scripts/Edit.php">
+     <input type="hidden" name="post_title" value= "'.$row[POST_TITLE].'">
+     <input type="hidden" name="post_time" value="'.$row[POST_TIME].'">
+     <input type="hidden" name="post_username" value="'.$row[POST_USERNAME].'">
+         <td><input type="submit" name="Edit" value = "Edit"></form></td>';
+         //Delete button
+         echo '<form method="post"action="http://cs2102-i.comp.nus.edu.sg/~a0101856/cs2102/scripts/Profile.php">
+     <input type="hidden" name="post_title" value= "'.$row[POST_TITLE].'">
+     <input type="hidden" name="post_time" value="'.$row[POST_TIME].'">
+     <input type="hidden" name="post_username" value="'.$row[POST_USERNAME].'">
+         <td><input type="submit" name ="Delete" value="Delete"></form></td>';
+         }
+         echo '</table>';
+         echo "</ul>";         
+
       //Find all comments on particular post
       $sql2 = "SELECT comment_username, comment_content, comment_time, count_votes FROM CONTAINS_COMMENTS WHERE post_title = '$row[POST_TITLE]' AND post_time = '$row[POST_TIME]' AND post_username = '$row[POST_USERNAME]'";
       //Number of comments for a post
