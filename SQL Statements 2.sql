e1bae425889b84e1f735c536f646606b41262330
@r-o-n-n <tanronney@users.noreply.github.com>
2015-04-13 22:56:21 +0800

    Update SQL Statements 2.sql

--- SQL Statements 2.sql
+++ SQL Statements 2.sql
@@ -2,19 +2,17 @@
 
 ------------------------
 
---test statements
 --Updating a particular post
   UPDATE post_writepost SET post_content = '' and post_title = '' WHERE post_username LIKE '' and post_time LIKE '';
 
 -------------------------------------------------
 
---test statements
 --voting post
   insert into vote_post(hasVoted,voter_username,post_username,post_title, post_time) values ('Y','sebbysebseb','sebbysebseb','Introduction post','15/01/01 01:25:25')
   
   UPDATE post_writepost SET pvotes = pvotes+1 WHERE post_username LIKE '' and post_time LIKE '';
 
---test statements
+
 --voting comment
   insert into vote_comment(hasVoted,voter_username,comment_time,comment_username,post_title,post_username,post_time) 
   values ('Y','sebbysebseb','15/01/01 01:25:25','sebbysebseb','Introduction post','sebbysebseb','15/01/01 01:25:25');
@@ -23,14 +21,12 @@
 
 ------------------------------------------------
   
---test statements
 --Decreasing the vote of a post
 --insert into vote_post(hasVoted,voter_username,post_username,post_title,post_time) values ('N','sebbysebseb','sebbysebseb','Introduction post','15/01/01 01:25:25')
   UPDATE vote_post SET hasVoted = 'N' WHERE post_time = '' and post_username = '' and voter_username = '';
   
   UPDATE post_writepost SET pvotes = pvotes-1 WHERE post_username LIKE '' and post_time LIKE '';
 
---test statements
 --Decreasing the vote of a comment
 --insert into vote_comment(hasVoted,voter_username,comment_time,comment_username,post_title,post_username,post_time) 
 --values ('N','sebbysebseb','15/01/01 01:25:25','sebbysebseb','Introduction post','sebbysebseb','15/01/01 01:25:25');
